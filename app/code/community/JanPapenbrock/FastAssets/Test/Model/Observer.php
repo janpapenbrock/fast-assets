ded2ce7080b594d9c2f0751cf1b3ef9fa8cf6c0e
janpapenbrock <lunoxx@gmail.com>
2014-03-21 23:52:01 +0100

    Add first (fake) test.

---
+++ app/code/community/JanPapenbrock/FastAssets/Test/Model/Observer.php
@@ -0,0 +1,15 @@
+<?php
+
+class JanPapenbrock_FastAssets_Test_Model_Observer extends EcomDev_PHPUnit_Test_Case
+{
+    /**
+     * Fake test to test if builds pass correctly.
+     *
+     * @test
+     */
+    public function fake()
+    {
+        $this->assertTrue(true);
+    }
+
+}
